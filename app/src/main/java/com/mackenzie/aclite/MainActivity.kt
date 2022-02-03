fd26d4420ae4310e15f6dd3a07141e88ef18260c
LuisMackenzie <principemackenzie@gmail.com>
2022-02-03 19:26:41 +0100

    Initial commit

---
+++ app/src/main/java/com/mackenzie/aclite/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.mackenzie.aclite
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.mackenzie.aclite.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpListeners()
+
+        viewModel.state.observe(this) { state ->
+            binding.etUser.error = state.userError?.let { getString(it) }
+            /*binding.etUser.error = state.userError?.let(this::getString)
+            binding.etUser.error = state.userError?.let(::getString)*/
+            binding.etPass.error = state.passError?.let(::getString)
+            binding.btnLogin.visibility = if (state.loggingIn) View.GONE else View.VISIBLE
+            binding.showProgress.visibility = if (state.loggingIn) View.VISIBLE else View.GONE
+
+            if (state.loggedIn) {
+                startActivity(Intent(this, NextActivity::class.java))
+                viewModel.onNavigateToNextScreen()
+                finish()
+            }
+
+        }
+
+    }
+
+    private fun setUpListeners() {
+
+        with(binding) {
+            btnLogin.setOnClickListener {
+                viewModel.onTryLogin(etUser.text.toString(), etPass.text.toString())
+            }
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/mackenzie/aclite/MainViewModel.kt
@@ -0,0 +1,55 @@
+package com.mackenzie.aclite
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel() {
+
+    private val _state = MutableLiveData<UiState>(UiState())
+    public val state:LiveData<UiState> get() = _state
+
+    public fun onTryLogin(user:String, pass: String) {
+        viewModelScope.launch {
+            _state.value = UiState(loggingIn = true)
+            tryLogin(user, pass)
+
+        }
+    }
+
+    private suspend fun tryLogin(user:String, pass:String) {
+        delay(2000)
+        val userError = if (!user.contains('@')) R.string.username_error else null
+        val passError = if (pass.length < 6) R.string.password_error else null
+        val loggedIn = userError == null && passError == null
+        _state.value = UiState(
+            loggedIn = loggedIn,
+            userError = userError,
+            passError = passError
+        )
+
+
+    }
+
+    fun onNavigateToNextScreen() {
+        _state.value = requireNotNull(_state.value).copy(loggedIn = false)
+    }
+
+    data class UiState(
+        val loggingIn:Boolean = false,
+        val loggedIn:Boolean = false,
+        val userError:Int? = null,
+        val passError:Int? = null
+        )
+
+
+    sealed class UiStateOld {
+        object Loading: UiStateOld()
+        data class Error(val userError: String, val passError:String): UiStateOld()
+        object LoggedIn: UiStateOld()
+    }
+
+}
